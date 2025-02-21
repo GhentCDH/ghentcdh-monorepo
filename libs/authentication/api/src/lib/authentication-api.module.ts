2400d38f60c9f90b7b7d19c828bf9b9df89dee37
Bo Vandersteene <bo@firepeak.tech>
2025-02-21 16:51:07 +0100

    feat(tools-api): add logging and health tooling

--- libs/authentication/api/src/lib/authentication-api.module.ts
+++ libs/authentication/api/src/lib/authentication-api.module.ts
@@ -2,8 +2,8 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { LoginController } from './login.controller';
 import { GhentCdhGuard } from './auth.guard';
+import { LoginController } from './login.controller';
 
 @Module({
   imports: [HttpModule, ConfigModule],
