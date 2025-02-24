c3bba5fbed25a593a7eebd408b80b2846e6bc945
Bo Vandersteene <bo@firepeak.tech>
2025-02-24 14:59:20 +0100

    feat(authentication/vue): update useHttpRequest

--- libs/authentication/vue/src/lib/keycloak.adapter.ts
+++ libs/authentication/vue/src/lib/keycloak.adapter.ts
@@ -1,35 +1,31 @@
 import Keycloak from 'keycloak-js';
 
-
 export class KeycloakAdapter extends Keycloak {
     private constructor() {
-        const {VITE_KEYCLOAK_REALM, VITE_KEYCLOAK_HOST, VITE_KEYCLOAK_CLIENT_ID} = import.meta.env;
+        const { VITE_KEYCLOAK_REALM, VITE_KEYCLOAK_HOST, VITE_KEYCLOAK_CLIENT_ID } =
+          import.meta.env;
 
         super({
             url: VITE_KEYCLOAK_HOST,
             realm: VITE_KEYCLOAK_REALM,
-            clientId: VITE_KEYCLOAK_CLIENT_ID
-        })
-
+            clientId: VITE_KEYCLOAK_CLIENT_ID,
+        });
     }
 
     private async initialize() {
         try {
-            const authenticated = await this.init(
-                {
-                    onLoad: 'login-required'
-                }
-            );
+            const authenticated = await this.init({
+                onLoad: 'login-required',
+            });
             if (authenticated) {
                 console.log('User is authenticated');
+            } else {
+                console.log('User is not authenticated');
             }
-            console.log('User is not authenticated');
-
         } catch (error) {
             console.error('Failed to initialize adapter:', error);
         }
     }
-
 
     static async init(): Promise<KeycloakAdapter> {
         const instance = new KeycloakAdapter();
@@ -38,13 +34,12 @@ export class KeycloakAdapter extends Keycloak {
         return instance;
     }
 
-
     get userInfo() {
-        return this.idTokenParsed
+        return this.idTokenParsed;
     }
 
     updateToken() {
-        return this.updateToken(30)
+        return this.updateToken(30);
     }
 
     get isAuthenticated() {
