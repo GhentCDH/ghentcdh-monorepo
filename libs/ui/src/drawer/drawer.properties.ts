292fd06a2e110d4a6dbb496d9055fe9e4d464d1d
Bo Vandersteene <bo@firepeak.tech>
2026-03-12 13:24:53 +0100

    feat: update drawer

---
+++ libs/ui/src/drawer/drawer.properties.ts
@@ -0,0 +1,6 @@
+export const DrawerProperties = {
+  widthLeft: { type: Number, default: 200 },
+  widthRight: { type: Number, default: 200 },
+  initialLeftOpen: { type: Boolean, default: true },
+  initialRightOpen: { type: Boolean, default: true },
+};
