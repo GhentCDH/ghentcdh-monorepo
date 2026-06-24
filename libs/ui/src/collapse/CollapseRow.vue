775076efdea65dda82f3b5c1474f5cf5591f9d04
Bo Vandersteene <bo@firepeak.tech>
2026-06-24 08:08:12 +0200

    feat: deprecate some modules

--- libs/ui/src/collapse/CollapseRow.vue
+++ libs/ui/src/collapse/CollapseRow.vue
@@ -4,29 +4,15 @@
       <div v-if="title">
         {{ title }}
       </div>
-      <div
-        v-if="description || descriptionLink"
-        class="text-xs text-gray-600"
-      >
-        <a
-          v-if="descriptionLink"
-          :href="descriptionLink"
-          target="_blank"
-        >
+      <div v-if="description || descriptionLink" class="text-xs text-gray-600">
+        <a v-if="descriptionLink" :href="descriptionLink" target="_blank">
           {{ description ?? descriptionLink }}
         </a>
         <span v-else>{{ description }}</span>
       </div>
     </div>
-    <div
-      v-if="label"
-      class="text-xs text-gray-500"
-    >
-      <a
-        v-if="labelLink"
-        :href="labelLink"
-        target="_blank"
-      >
+    <div v-if="label" class="text-xs text-gray-500">
+      <a v-if="labelLink" :href="labelLink" target="_blank">
         {{ label }}
       </a>
       <span v-else>{{ label }}</span>
--- libs/ui/src/shell/shell.component.vue
+++ libs/ui/src/shell/shell.component.vue
@@ -4,7 +4,7 @@
   <div class="flex h-screen flex-col">
     <header class="shrink-0">
       <nav class="navbar bg-primary text-primary-content w-full z-100">
-        <div class="navbar-start">
+        <div class="navbar-start" v-if="menu?.length">
           <button
             aria-label="open sidebar"
             class="btn btn-square btn-ghost p-1"
@@ -17,14 +17,11 @@
           <a
             class="btn btn-ghost text-primary-content text-lg color-white"
             :href="baseUrl"
-          >{{ title }}</a>
+            >{{ title }}</a
+          >
         </div>
         <div class="navbar-end">
-          <SubMenu
-            v-if="userMenu"
-            v-bind="userMenu"
-            :btn-class="''"
-          />
+          <SubMenu v-if="userMenu" v-bind="userMenu" :btn-class="''" />
         </div>
       </nav>
     </header>
@@ -37,7 +34,7 @@
         v-model="sidebarOpen"
         type="checkbox"
         class="drawer-toggle"
-      >
+      />
       <main class="drawer-content overflow-y-auto mb-2 mr-2 shadow-md">
         <slot />
       </main>
@@ -52,18 +49,13 @@
           :style="{ width: widthSidebar + 'px' }"
         >
           <ul class="flex-grow overflow-y-auto">
-            <li
-              v-for="item of menu"
-              :key="item.label"
-            >
+            <li v-for="item of menu" :key="item.label">
               <RouterLink :to="{ name: item.routerLink, params: item.params }">
                 {{ item.label }}
               </RouterLink>
             </li>
           </ul>
-          <div class="bg-base-200 p-4">
-            version: {{ version }}
-          </div>
+          <div class="bg-base-200 p-4">version: {{ version }}</div>
         </div>
       </div>
     </div>
